7404ba89fd4324849ee5548b9bee19991cb674fa
David Trujillo Carrero <davitru60@gmail.com>
2023-09-21 13:15:14 +0200

    primer commit

---
+++ app.js
@@ -0,0 +1,111 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const email = document.getElementById("email")
+    const password = document.getElementById("password")
+    const form = document.getElementById("form-container")
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const inputEmail = email.value
+        const inputPasswd = password.value
+        const validatedCredentials = validateFormFields(inputEmail, inputPasswd)
+        
+
+        if (validatedCredentials.isValid) {
+            formRedirection()
+        } else {
+            alert(validatedCredentials.errors.join("\n"))
+        }
+    })
+
+    function emailFormat(email) {
+        var isValid=true
+
+        // Comprobar que el email contenga al menos un "@".
+        if (email.indexOf('@') === -1) {
+            isValid=false;
+        }
+    
+        // Comprobar que "@" no esté en el primer carácter ni en los últimos tres caracteres.
+        if (email.indexOf('@') === 0 || email.indexOf('@') >= email.length - 3) {
+            isValid=false;
+        }
+
+        console.log(email)
+    
+        return isValid;
+    }
+
+    function isEmptyEmail(email){
+        return email.length==0
+    }
+
+    function checkUser(email,passwd){
+        var correctEmail="dtc138@gmail.com"
+        var correctPasswd="admin123"
+        var isValid=false
+
+        if(email==correctEmail && passwd==correctPasswd){
+            isValid=true
+        }else{
+            isValid=false
+        }
+
+        return isValid
+    }
+
+    function isEmptyPasswd(passwd){
+        console.log(passwd)
+        return passwd.length==0
+    }
+
+    function passwdChar(passwd){
+        return passwd.length<8
+    }
+
+
+    function validateFormFields(email, passwd) {
+        const errors = []
+        
+        if (isEmptyEmail(email)) {
+            errors.push("Email vacío")
+        }
+
+        if (isEmptyPasswd(passwd)) {
+            errors.push("Contraseña vacía")
+        }
+
+        if (passwdChar(passwd)) {
+            errors.push("La contraseña debe tener al menos ocho caracteres")
+        }
+
+        if(!emailFormat(email)){
+            errors.push("El formato del correo electrónico es incorrecto")
+        }
+
+        if(!checkUser(email,passwd)){
+            errors.push("Usuario incorrecto")
+        }
+
+        console.log(errors)
+
+        return errorCollector(errors)
+        
+    }
+
+    function errorCollector(errors){
+        var errorObject={isValid:false,errors:[]}
+
+        if(errors.length==0){
+            errorObject={isValid:true, errors:[]}
+        }else{
+            errorObject={isValid: false, errors: errors};
+        }
+
+        return errorObject
+    }
+
+
+    function formRedirection() {
+        window.location.href = "formulario2.html"
+    }
+})
---
+++ app2.js
@@ -0,0 +1,122 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const form = document.getElementById("form-container")
+    const clearButton = document.getElementById("clear-btn");
+
+    const name = document.getElementById("name")
+    const firstSurname = document.getElementById("first-surname")
+    const secondSurname = document.getElementById("second-surname")
+    const birthDate = document.getElementById("birth-date")
+    const id = document.getElementById("id")
+
+/**
+ * Cuando se produce el evento submit al pulsar el boton de guardar 
+ * se realizan las acciones de comprobacion 
+ */
+    form.addEventListener("submit", handleFormSubmit)
+
+    function handleFormSubmit(e){
+        e.preventDefault()
+        const inputName = name.value
+        const inputFirstSurname = firstSurname.value
+        const inputSecondSurname = secondSurname.value;
+        const inputBirthDate = birthDate.value
+        const inputID = id.value
+
+        const inputData = {
+            "name": inputName,
+            "surname1": inputFirstSurname,
+            "surname2": inputSecondSurname,
+            "birthDate": inputBirthDate,
+            "id": inputID
+        }
+
+        const validatedFields = validateFormFields(inputData);
+       
+        if(validatedFields.isValid){
+            const persona=inputData
+            console.log(persona)
+        }else{
+            alert(validatedFields.errors.join("\n"))
+        }
+    }
+
+   
+    function checkName(name){
+        return name.length < 3 || name.length > 30;
+    }
+
+    function checkSurname(surname){
+        return surname.length <2 || surname.length>30
+    }
+
+    function checkID(id){
+        var isValid=false
+
+        if(id.length !=9){
+            isValid=false
+        }
+
+        const letters = "TRWAGMYFPDXBNJZSQVHLCKE";
+        const number = id.slice(0,8)
+        const idLetter = id.charAt(8) 
+
+        const reminder = number % 23
+
+        const correctLetter= letters.charAt(reminder)
+
+        if(idLetter==correctLetter){
+            isValid=true
+        }else{
+            isValid=false
+        }
+
+        return isValid
+    
+    }
+
+    function validateFormFields(formData) {
+        const errors = [];
+
+        if (checkName(formData.name)) {
+            errors.push("El nombre debe tener entre 3 y 30 caracteres");
+        }
+
+        if(checkSurname(formData.surname1)){
+            errors.push("El primer apellido debe tener entre 2 y 30 caracteres");
+        }
+
+        if(checkName(formData.surname2)){
+            errors.push("El segundo apellido debe tener entre 2 y 30 caracteres");
+        }
+
+        if(!checkID(formData.id)){
+            errors.push("DNI no valido")
+        }
+
+
+        return errorCollector(errors)
+    }
+
+    function errorCollector(errors){
+        var errorObject={isValid:false,errors:[]}
+
+        if(errors.length==0){
+            errorObject={isValid:true, errors:[]}
+        }else{
+            errorObject={isValid: false, errors: errors};
+        }
+
+        return errorObject
+    }
+
+    function clearForm(){
+        form.reset()
+    }
+
+    /**
+     * Cuando se pulsa el boton de limpiar se borran los datos del campo
+     */
+    clearButton.addEventListener("click", clearForm);
+
+})
